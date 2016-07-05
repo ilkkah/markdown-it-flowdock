6ae2cb4128a56ca3a26af158adbde5b7fcd05f11
Ilkka Huotari <ilkkah@gmail.com>
2016-07-06 02:41:17 +0300

    add hrefs to links

--- src/hashtag.js
+++ src/hashtag.js
@@ -3,12 +3,19 @@ import parser from './parser';
 function flowdockHashtag(tokens, idx) {
   var tag = tokens[idx].content;
   var markup = tokens[idx].markup;
-  return '<a class="tag">' + markup + tag + '</a>';
+  var result = '<a class="tag"';
+  if (flowdockHashtag.options && flowdockHashtag.options.href) {
+	result += ' href="' + flowdockHashtag.options.href + tag + '"';
+  }
+  result += '>' + markup + tag + '</a>';
+
+  return result;
 }
 
 export default function(md, options) {
   const split = "#|＃";
   const hashtag = parser(md, 'hashtag', new RegExp(split));
   md.core.ruler.push('hashtag', hashtag);
+  flowdockHashtag.options = options && options.hashtags;
   md.renderer.rules.hashtag = flowdockHashtag;
 };
--- src/mention.js
+++ src/mention.js
@@ -3,12 +3,19 @@ import parser from './parser';
 function flowdockMention(tokens, idx) {
   var tag = tokens[idx].content;
   var markup = tokens[idx].markup;
-  return '<a class="mention">' + markup + tag + '</a>';
+  var result = '<a class="mention"';
+  if (flowdockMention.options && flowdockMention.options.href) {
+	result += ' href="' + flowdockMention.options.href + tag + '"';
+  }
+  result += '>' + markup + tag + '</a>';
+
+  return result;
 }
 
 export default function(md, options) {
   const split = "@|＠";
   const mention = parser(md, 'mention', new RegExp(split));
   md.core.ruler.push('mention', mention);
+  flowdockMention.options = options && options.mentions;
   md.renderer.rules.mention = flowdockMention;
 };
