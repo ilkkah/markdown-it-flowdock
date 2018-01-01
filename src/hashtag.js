30d2d3b2145e563a2adf9983f51e405d853957cc
Ilkka Huotari <ilkkah@gmail.com>
2018-01-01 08:36:33 +0200

    opentask js call

--- src/hashtag.js
+++ src/hashtag.js
@@ -8,7 +8,7 @@ function flowdockHashtag(tokens, idx) {
   var result = '<a class="tag"';
   if (flowdockHashtag.options && flowdockHashtag.options.pid && (flowdockHashtag.options.href_channel || flowdockHashtag.options.href_task)) {
     if (intRegex.test(tag) && tag !== '0') {
-      result += ' data-norender="1" on-click="helpers.showTask(' + tag + ')" href="' + flowdockHashtag.options.href_task + '/' + flowdockHashtag.options.pid + '/' + tag + '"';
+      result += ' data-norender="1" onclick="showTaskByNum(event, ' + tag + ',\'' + flowdockHashtag.options.pid + '\')" href="' + flowdockHashtag.options.href_task + flowdockHashtag.options.pid + '/' + tag + '"';
     }
     else {
       result += ' href="' + flowdockHashtag.options.href_channel + tag + '"';
