2320d005c81aabf19136788fb226f4f88ddb041a
Ilkka Huotari <ilkkah@gmail.com>
2018-01-01 08:35:36 +0200

    opentask js call

--- src/hashtag.js
+++ src/hashtag.js
@@ -1,11 +1,18 @@
 import parser from './parser';
 
+var intRegex = /^\d+$/;
+
 function flowdockHashtag(tokens, idx) {
   var tag = tokens[idx].content;
   var markup = tokens[idx].markup;
   var result = '<a class="tag"';
-  if (flowdockHashtag.options && flowdockHashtag.options.href) {
-	result += ' href="' + flowdockHashtag.options.href + tag + '"';
+  if (flowdockHashtag.options && flowdockHashtag.options.pid && (flowdockHashtag.options.href_channel || flowdockHashtag.options.href_task)) {
+    if (intRegex.test(tag) && tag !== '0') {
+      result += ' data-norender="1" on-click="helpers.showTask(' + tag + ')" href="' + flowdockHashtag.options.href_task + '/' + flowdockHashtag.options.pid + '/' + tag + '"';
+    }
+    else {
+      result += ' href="' + flowdockHashtag.options.href_channel + tag + '"';
+    }
   }
   result += '>' + markup + tag + '</a>';
 
