97f8458290344305538f403616a5a01befbd62c3
Ilkka Huotari <ilkkah@gmail.com>
2018-01-10 23:44:48 +0200

    ..

--- src/hashtag.js
+++ src/hashtag.js
@@ -5,13 +5,13 @@ var intRegex = /^\d+$/;
 function flowdockHashtag(tokens, idx) {
   var tag = tokens[idx].content;
   var markup = tokens[idx].markup;
-  var result = '<a class="tag"';
+  var result = '<a ';
   if (flowdockHashtag.options && flowdockHashtag.options.pid && (flowdockHashtag.options.href_channel || flowdockHashtag.options.href_task)) {
     if (intRegex.test(tag) && tag !== '0') {
-      result += ' data-norender="1" onclick="showTaskByNum(event, ' + tag + ',\'' + flowdockHashtag.options.pid + '\')" href="' + flowdockHashtag.options.href_task + flowdockHashtag.options.pid + '/' + tag + '"';
+      result += ' class="task" data-norender="1" onclick="showTaskByNum(event, ' + tag + ',\'' + flowdockHashtag.options.pid + '\')" href="' + flowdockHashtag.options.href_task + flowdockHashtag.options.pid + '/' + tag + '"';
     }
     else {
-      result += ' href="' + flowdockHashtag.options.href_channel + tag + '"';
+      result += ' class="tag" href="' + flowdockHashtag.options.href_channel + tag + '"';
     }
   }
   result += '>' + markup + tag + '</a>';
