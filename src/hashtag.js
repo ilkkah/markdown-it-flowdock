491f1e69185600c363514bd47233d8e582829309
Ilkka Huotari <ilkkah@gmail.com>
2018-02-25 09:34:02 +0200

    ..

--- src/hashtag.js
+++ src/hashtag.js
@@ -1,17 +1,20 @@
 import parser from './parser';
 
 var intRegex = /^\d+$/;
+var taskAndProjectRegex = /^([a-z][a-z0-9-]+)_(\d+)$/;
 
 function flowdockHashtag(tokens, idx) {
   var tag = tokens[idx].content;
   var markup = tokens[idx].markup;
   var result = '<a ';
-  if (flowdockHashtag.options && flowdockHashtag.options.pid && (flowdockHashtag.options.href_channel || flowdockHashtag.options.href_task)) {
-    if (intRegex.test(tag) && tag !== '0') {
+  if (flowdockHashtag.options && (flowdockHashtag.options.href_channel || flowdockHashtag.options.href_task)) {
+    if (intRegex.test(tag) && tag !== '0' && flowdockHashtag.options.pid) {
       result += 'class="task" data-tasknum="' + tag + '" data-taskpid="' + flowdockHashtag.options.pid + '"';
     }
-    else {
-      result += 'class="tag" href="' + flowdockHashtag.options.href_channel + encodeURIComponent('#'+tag) + '"';
+    else if (taskAndProjectRegex.test(tag)) {
+      result += 'class="task" data-task="' + tag + '"';
+    } else {
+      result += 'class="tag" href="' + flowdockHashtag.options.href_channel + encodeURIComponent('#' + tag) + '"';
     }
   }
   result += '>' + markup + tag + '</a>';
